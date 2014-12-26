9da68bb08255ab9c5042930ece1246a2564e9e23
geo <geomo3@gmail.com>
2014-12-26 11:26:19 -0600

    Major changes. Synced this repo with newest Club Hub from old repo. Removed the Bakery. Changed the introduction text. New logos.

--- thebakery/meta/bakerytemplate.js
+++
@@ -1,6 +0,0 @@
-if(!window.template)
-{
-        console.log("Creating template variable to store template.js plugin locations.");
-        window.template = [];
-}
-template.push("/thebakery/meta/getproject-template.js");
--- thebakery/meta/menuloader.js
+++
@@ -1,3 +0,0 @@
-$( document ).ready(function(){
-        $("#main-menu").load("/thebakery/meta/main-menu.html");
-});
